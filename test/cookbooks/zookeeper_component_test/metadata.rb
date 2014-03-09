65eb90759f9cb1baafe0ce4171186efc99f3a809
Anton Butovich <abutovich@gmail.com>
2014-03-09 13:54:21 +0200

    added tests

---
+++ test/cookbooks/zookeeper_component_test/metadata.rb
@@ -0,0 +1,5 @@
+name "zookeeper_component_test"
+version "0.1.0"
+
+depends "zookeeper-component"
+depends "minitest-handler"
---
+++ test/cookbooks/zookeeper_component_test/recipes/default.rb
@@ -0,0 +1,2 @@
+include_recipe "zookeeper"
+include_recipe "minitest-handler"
